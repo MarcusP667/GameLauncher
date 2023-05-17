032195c682c8cd3edb1bbbbdc3cbb29b258f7e30
MarcusP667 <m.pfennig@ustw.de>
2023-05-17 14:07:26 +0200

    Update stand 17.05.23 14:07

--- GameLauncher/src/GameLauncher.java
+++ GameLauncher/src/GameLauncher.java
@@ -1,61 +1,106 @@
-import java.awt.*;
-import java.awt.event.*;
 
-import javax.swing.*;
+
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 
 public class GameLauncher implements ActionListener  {
 
-	JFrame frame;
-	JPanel panel;
-	JButton game1Button, game2Button;
+		JFrame frame;
+		JPanel panel;
+		JButton game1Button, game2Button;
 	
-	public GameLauncher() {
+		public GameLauncher() {
 		
-		// Create HomeScreen and Panel
+		
+		
+		// Erstelle HomeScreen and Panel
 		frame = new JFrame ("Game Launcher");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		
+		
+		
 		// Erstelle Schaltflächen für die einzelnen Spiele 
 		
-		game1Button = new JButton("TIC-TAC-TOE");
+		game1Button = createGameButton("TIC-TAC-TOE", "pfad zum png");
 		game1Button.addActionListener(this);
 		panel.add(game1Button, BorderLayout.NORTH);
 		
-		game2Button = new JButton("PAC-MAN");
+		game2Button =  createGameButton("PAC-MAN", "C:\\Users\\49152\\Documents\\GitHub\\GameLauncher\\GameLauncher\\Image");
 		game2Button.addActionListener(this);
 		panel.add(game2Button, BorderLayout.NORTH);
 		
+		frame.getContentPane().add(panel);
+		frame.setSize(300, 200);
+		frame.setVisible(true);
 	}
-	
-	public static void main(String[] args) {
 		// Erstellung einer Instanz des GemaLauncher
+		public static void main(String[] args) {
 		new GameLauncher();
-	}
-	
-	public void  actionPerformend(ActionEvent e) {
-		// Überprüfung welcher Knopf gedrückt wurde und Starte die Anwendung
 		
-		if (e.getSource()== game1Button) {
-			// Starte das erste Spiel
-			System.out.println("TIC-TAC-TOE wird gestartet...");
+		}
 		
-		if (e.getSource()== game2Button) {
-			// Sarte das zweite Spiel
-			System.out.println("PAC-MAN wird gestartet...");
+		
+		
+		@Override
+		
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource()== game1Button) {
+				// Hier Startet TIC-TAC-TOE
+				startGame1();
 			
-		}
-		}
-	}
+			}	else if (e.getSource()== game2Button) {
+					// Hier Sartet PAC-MAN
+					startGame2();
+				}
+			}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+		
+		// Methode zum Erstellen von Buttons mit Text und Bild
+		
+		private JButton createGameButton(String buttonText, String imagePath) {
+			JButton button = new JButton(buttonText);
+			button.addActionListener(this);
+			
+			// Setze das Bild für den Button
+			ImageIcon icon = new ImageIcon(imagePath);
+			button.setIcon(icon);
+			
+			return button;
+		}
+		
+		
+		
+		// Methode zum Starten von TIC-TAC_TOE
+	
+		private void startGame1() {
+		
+			// Hier einen verweis auf TIC-TAC-TOE einbauen
+		
+		JOptionPane.showMessageDialog(frame, "TIC-TAC-TOE wird gestartet....");
 	}
 	
+
+		
+		// Methode zum Starten von PAC-AMN
 	
+		private void startGame2() {
+			
+			// Hier einen verweis auf PAc-MAN einbauen
+		
+		JOptionPane.showMessageDialog(frame, "PAC-MAN wird gestartet....");
+	}
 	
 }
  
